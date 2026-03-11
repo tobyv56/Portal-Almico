be8c9189b29abafe8cb1e63a13c5615f71d08a7e
toby_velez <115357174+tobyv56@users.noreply.github.com>
2026-03-11 11:46:23 -0300

    Refactor FastAPI application structure

--- config.py
+++ config.py
@@ -70,11 +70,7 @@ async def creacion_cursos(nombreTaller: Annotated[str, Form()],
     ruta_carpeta = "static/imagenes"
     os.makedirs(ruta_carpeta, exist_ok=True) 
     
-    ruta_archivo = os.path.join(ruta_carpeta, archivo.filename)
-                              
-    with open(ruta_archivo, "wb") as buffer:
-        shutil.copyfileobj(archivo.file, buffer)
-
+    
     cursor.execute(query,(nombreTaller,fecha,hora,descripcion))
     conn.commit()
 
@@ -130,5 +126,6 @@ def proceso_turno(nombre: Annotated[str, Form()],
 
 
     
+
 
 
