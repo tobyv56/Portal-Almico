69e1a2cbee0f2d57efcd9c99cb854b0e102ef0c0
Velez Tobias <115357174+tobyv56@users.noreply.github.com>
2026-08-03 00:17:41 -0300

    refactor

--- config.py
+++ config.py
@@ -1,383 +1,47 @@
-from fastapi import FastAPI, Form, Request, UploadFile, File
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from typing import Annotated
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import os
-import uuid
-import shutil
-from datetime import date, time
-from apscheduler.schedulers.background import BackgroundScheduler
-from dotenv import load_dotenv
 from pathlib import Path
 
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
+from routers.paginas import router as paginas_router
+from routers.talleres import router as talleres_router
+from routers.usuarios import router as usuarios_router
+from routers.reservas import router as reservas_router
+
 BASE_DIR = Path(__file__).resolve().parent
+
 load_dotenv(BASE_DIR / ".env", override=True)
 
-import cloudinary
-import cloudinary.uploader
+app = FastAPI()
 
-CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
-CLOUD_API_KEY = os.getenv("CLOUDINARY_API_KEY")
-CLOUD_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
-print("Cloud name cargado:", bool(CLOUD_NAME))
-print("API key cargada:", bool(CLOUD_API_KEY))
-print("API secret cargado:", bool(CLOUD_API_SECRET))
+SESSION_SECRET = os.getenv("SESSION_SECRET")
 
-cloudinary.config(
-    cloud_name=CLOUD_NAME,
-    api_key=CLOUD_API_KEY,
-    api_secret=CLOUD_API_SECRET,
-    secure=True
+if not SESSION_SECRET:
+    raise RuntimeError("Falta SESSION_SECRET en el archivo .env")
+
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=SESSION_SECRET,
+    same_site="lax",
+    https_only=False
 )
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-def get_db():
-    conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    return conn, cursor
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "static")),
+    name="static"
+)
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.include_router(paginas_router)
+app.include_router(talleres_router)
+app.include_router(usuarios_router)
+app.include_router(reservas_router)
 
-scheduler = BackgroundScheduler()
-
-def eliminar_talleres_viejos():
-    print("Limpiando talleres...")
-
-    conn, cursor = get_db()
-
-    cursor.execute("""
-        DELETE FROM curso
-        WHERE (fecha + hora) < CURRENT_TIMESTAMP;
-    """)
-
-    print(f"Eliminados: {cursor.rowcount}")
-
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-scheduler.add_job(
-    eliminar_talleres_viejos,
-    "cron",
-    minute=0,
-    hour=0)
-
-scheduler.start()
-
-@app.get("/reservas") 
-async def mostrar_reservas(request: Request):
-    return templates.TemplateResponse( 
-            request=request,
-            name="reservas.html",
-            context={"request": request})
-
-@app.get("/", response_class=HTMLResponse) 
-def mostrar_inicio(
-    request: Request,
-    mensaje: str | None = None,
-    tipo: str | None = None
-):
-    conn, cursor = get_db()
-
-    try:
-        cursor.execute("""SELECT nombrecurso,mes,imagen FROM curso
-                      LIMIT 3""")
-    
-        talleres = cursor.fetchall()
-
-        return templates.TemplateResponse(
-            request=request,
-            name="index.html",
-            context={
-                "talleres": talleres,
-                "mensaje": mensaje,
-                "tipo": tipo
-            }
-        )
-    finally:
-            cursor.close()
-            conn.close()
-
-@app.get("/cursos", response_class=HTMLResponse)
-def mostrar_cursos(
-    request: Request,
-    mensaje: str | None = None,
-    tipo: str | None = None
-):
-    conn, cursor = get_db()
-
-    try:
-        cursor.execute("""
-            SELECT *
-            FROM curso
-        """)
-
-        talleres = cursor.fetchall()
-
-        return templates.TemplateResponse(
-            request=request,
-            name="cursos.html",
-            context={
-                "talleres": talleres,
-                "mensaje": mensaje,
-                "tipo": tipo
-            }
-        )
-
-    finally:
-        cursor.close()
-        conn.close()
-
-@app.post("/cursos/eliminacion")
-def eliminar_taller(
-    nombretaller: Annotated[str, Form()]
-):
-    conn, cursor = get_db()
-
-    try:
-        cursor.execute(
-            """
-            DELETE FROM curso
-            WHERE nombrecurso = %s
-            """,
-            (nombretaller,)
-        )
-
-        if cursor.rowcount == 0:
-            conn.rollback()
-
-            return RedirectResponse(
-                url="/cursos?mensaje=Taller+no+encontrado&tipo=warning",
-                status_code=303
-            )
-
-        conn.commit()
-
-        return RedirectResponse(
-            url="/cursos?mensaje=Taller+eliminado+correctamente&tipo=success",
-            status_code=303
-        )
-
-    except Exception as error:
-        conn.rollback()
-        print("Error al eliminar:", error)
-
-        return RedirectResponse(
-            url="/cursos?mensaje=No+se+pudo+eliminar+el+taller&tipo=error",
-            status_code=303
-        )
-
-    finally:
-        cursor.close()
-        conn.close()
-
-@app.post("/cursos/creacion", response_class=HTMLResponse)
-def crear_taller(
-    request: Request,
-    nombrecurso: Annotated[str, Form()],
-    mes: Annotated[str, Form()],
-    descripcion: Annotated[str, Form()],
-    imagen: Annotated[UploadFile, File()]
-):
-    conn, cursor = get_db()
-
-    try:
-        resultado = cloudinary.uploader.upload(
-            imagen.file,
-            folder="portal-almico/cursos",
-            resource_type="image"
-        )
-
-        url_imagen = resultado["secure_url"]
-
-        cursor.execute(
-            """
-            INSERT INTO curso
-                (nombrecurso, mes, descripcion, imagen)
-            VALUES
-                (%s, %s, %s, %s)
-            """,
-            (
-                nombrecurso,
-                mes,
-                descripcion,
-                url_imagen
-            )
-        )
-
-        conn.commit()
-
-        return RedirectResponse(
-            url="/cursos",
-            status_code=303
-        )
-
-    except Exception as error:
-        conn.rollback()
-
-        print("Error al crear el taller:", error)
-
-        return RedirectResponse(
-            url="/cursos/creacion",
-            status_code=303
-        )
-
-    finally:
-        cursor.close()
-        conn.close()
-
-@app.get("/presentacion") 
-async def mostrar_presentacion(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="quienes-somos.html",
-        context={"request": request})
-
-@app.get("/ubicacion") 
-async def mostrar_ubicacion(request: Request):
-    return templates.TemplateResponse(request=request,
-                                      name="ubicacion.html", 
-                                      context={"request": request})
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-@app.post("/registro", response_class=HTMLResponse)
-def proceso_turno(
-    request: Request,
-    nombre: Annotated[str, Form()] = None,
-    numTelefono: Annotated[str, Form()] = None,
-    email: Annotated[str, Form()] = None,
-    profesor: Annotated[str, Form()] = None,
-    actividad: Annotated[str, Form()] = None,
-    fecha: Annotated[str | None, Form()] = None,
-    horario: Annotated[str | None, Form()] = None,
-):
-    # validacion horario
-    if not horario:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "seleccione el horario.",
-                "tipo": "error"
-            }
-    )
-    
-    # validacion fecha
-    if not fecha:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "seleccione la fecha.",
-                "tipo": "error"
-            }
-    )
-
-    # validacion profesor 
-    if not profesor:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "seleccione el profesor/a.",
-                "tipo": "error"
-            }
-    )
-
-    # validacion actividad
-    if not actividad:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "seleccione la actividad.",
-                "tipo": "error"
-            }
-    )
-
-    # Validación del nombre
-    if not nombre or not nombre.strip():
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "El nombre no puede estar vacío.",
-                "tipo": "error"
-            }
-    )
-
-    # Validación del teléfono
-    
-    if not numTelefono or not numTelefono.strip():
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "El número de teléfono no puede estar vacío.",
-                "tipo": "warning"
-            }
-        )
-    
-    if not numTelefono.isdigit() or len(numTelefono) != 10:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "El número de teléfono es inválido.",
-                "tipo": "warning"
-            }
-        )
-    
-    # Validación del email
-    
-    if not email or not email.strip():
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "El correo no puede estar vacío.",
-                "tipo": "warning"
-            }
-        )
-    
-    if "@gmail.com" not in email and "@hotmail.com" not in email:
-        return templates.TemplateResponse(
-            request=request,
-            name="reservas.html",
-            context={
-                "request": request,
-                "mensaje": "El email es inválido.",
-                "tipo": "error"
-            }
-        )
-
-    return templates.TemplateResponse(
-        request=request,
-        name="reservas.html",
-        context={
-            "request": request,
-            "mensaje": f"¡Genial {nombre}! Tu turno fue registrado correctamente.",
-            "tipo": "success"
-        }
-    )
 
     
     
