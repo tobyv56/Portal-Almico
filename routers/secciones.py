e64d56ca3db065e783309126501bbab6984a7a9e
Velez Tobias <115357174+tobyv56@users.noreply.github.com>
2026-09-08 22:58:54 -0300

    add domain seccion

---
+++ routers/secciones.py
@@ -0,0 +1,168 @@
+from typing import Annotated
+from routers.usuarios import Usuario
+
+import cloudinary.uploader
+
+from fastapi import (
+    APIRouter,
+    File,
+    Form,
+    Request,
+    UploadFile,
+    Depends,
+    HTTPException
+)
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from configuracion import templates
+from database import get_db
+from routers.usuarios import (Usuario,obtener_usuario_actual)
+
+router = APIRouter(
+    tags=["Talleres"]
+)
+
+def contexto_sesion(request: Request):
+    id_usuario = request.session.get("idusuario")
+    rol_usuario = request.session.get("rol")
+
+    return {
+        "idusuario": id_usuario,
+        "rol_usuario": rol_usuario,
+        "usuario_logueado": id_usuario is not None,
+        "es_admin": rol_usuario == "admin"
+    }
+
+@router.get("/infoSecciones", response_class=HTMLResponse)
+def mostrar_ubicacion(request: Request):
+
+    conn, cursor = get_db()
+
+    try:
+
+        repositorio = RepositorioSeccion(cursor)
+
+        secciones = repositorio.obtener_todos()
+
+        print(secciones)
+
+        return templates.TemplateResponse(
+            request=request,
+            name="infoSecciones.html",
+            context={
+                **contexto_sesion(request),
+                "secciones": secciones
+            }
+        )
+
+    finally:
+        cursor.close()
+        conn.close()
+
+@router.post("/creacionSeccion")
+def crearSeccion(nombre: Annotated[str, Form()],
+                 descripcion: Annotated[str, Form()],
+                 imagen: Annotated[UploadFile, File()],
+                 usuarioActual:Annotated[
+                         Usuario,
+                         Depends(obtener_usuario_actual)
+                     ]):
+
+    conn, cursor = get_db()
+
+    if not usuarioActual.tiene_permisos():
+        return RedirectResponse(
+            url="/cursos?mensaje=No+tenes+permisos&tipo=error",
+            status_code=303
+        )
+
+    repositorioSeccion = RepositorioSeccion(cursor)
+    resultado = cloudinary.uploader.upload(
+                imagen.file,
+                folder="portal-almico/creacionSeccion",
+                resource_type="image"
+            )
+
+    #print("RESULTADO CLOUDINARY:", resultado)
+    #print("URL:", resultado["secure_url"])
+
+    seccion = Seccion(
+        nombre= nombre,
+        descripcion= descripcion,
+        imagen = resultado["secure_url"]
+    )
+
+    repositorioSeccion.crearSeccion(seccion)
+
+    conn.commit()
+    
+    return RedirectResponse(
+                url=(
+                    "/infoSecciones"
+                    "?mensaje=Seccion+creado+correctamente"
+                    "&tipo=success"
+                ),
+                status_code=303
+            )
+
+class Seccion:
+
+    def __init__(
+        self,
+        nombre,
+        descripcion,
+        imagen
+    ):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.imagen = imagen
+
+    def validar(self):
+            if not self.nombre or not self.nombre.strip():
+                raise ValueError("El nombre de la seccion no puede estar vacío")
+    
+            if not self.descripcion or not self.descripcion.strip():
+                raise ValueError("La descripción no puede estar vacía")
+    
+            if not self.imagen or not self.url_imagen.strip():
+                raise ValueError("La imagen no puede estar vacía")
+
+class RepositorioSeccion:
+
+    def __init__(self, cursor):
+            self.cursor = cursor
+    
+    def obtener_todos(self):
+            self.cursor.execute("""
+                SELECT *
+                FROM seccion
+                ORDER BY idseccion
+            """)
+    
+            return self.cursor.fetchall()
+
+    def crearSeccion(self,seccion):
+            self.cursor.execute(
+                        """
+                        INSERT INTO seccion
+                            (nombreseccion, descripcion, imagen)
+                        VALUES
+                            (%s, %s, %s)
+                        """,
+                        (
+                            seccion.nombre,
+                            seccion.descripcion,
+                            seccion.imagen
+                        )
+                    )
+    
+    def eliminacion_taller(self, nombreSeccion):
+            self.cursor.execute(
+                """
+                DELETE FROM seccion
+                WHERE nombrecurso = %s
+                """,
+                (nombreSeccion,)
+                )
+    
+            return self.cursor.rowcount > 0
