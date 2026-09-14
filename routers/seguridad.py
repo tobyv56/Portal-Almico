60c04da652cf32517b3c943ef4afe9ca344f3e81
Velez Tobias <115357174+tobyv56@users.noreply.github.com>
2026-09-14 10:24:40 -0300

    documentacion rt seguridad

--- routers/seguridad.py
+++ routers/seguridad.py
@@ -1,12 +1,12 @@
 from pwdlib import PasswordHash
 
-password_hash = PasswordHash.recommended()
+password_hash = PasswordHash.recommended() #crea un objeto segun las recomendaciones de la libreria para hashear la contra
 
-def hashear_contrasena(contrasena: str) -> str:
-    return password_hash.hash(contrasena)
+def hashear_contrasena(contrasena: str) -> str: #dice que esta pensando para un str 
+    return password_hash.hash(contrasena) #hashea la contrasena
 
 def verificar_contrasena( contrasena_ingresada: str, contrasena_hasheada: str) -> bool:
-    return password_hash.verify(
+    return password_hash.verify( #compara con la contrasena normal con la hasheada
         contrasena_ingresada,
         contrasena_hasheada
     )
